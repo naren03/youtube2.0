492a9615921f776a5d41ead2592f9a35be967083
Narendra Singh Rathore <65424020+naren03@users.noreply.github.com>
2025-04-29 09:39:42 +0530

    Create src/test/java/hooks/Hooks.java

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,23 @@
+package hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hooks {
+    public static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
